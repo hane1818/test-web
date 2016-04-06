f1ef90d91f3cc3c788b5c604ee1e0151cb3b7425
Hane Liu <hane0131@gmail.com>
2016-04-06 15:15:55 +0800

    Add random sentence

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
+def home(request):
+    sentence = ['random1', 'random2', 'random3']
+    return render(request, 'index.html', {
+        'sentence': random.choice(sentence),
+    })
